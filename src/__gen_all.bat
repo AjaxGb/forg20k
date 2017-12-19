eab9f108cb5bf0e60a1f73a29ce01b769af4d4a6
AjaxGb <ajaxgb@gmail.com>
2017-12-19 08:42:51 -0500

    More user-friendly generation script

--- src/__gen_all.bat
+++ src/__gen_all.bat
@@ -1,4 +1,11 @@
+@echo off
+mkdir gen 2> nul
+
+echo **GENERATING SPRITE SHEET
 python _sheetbuilder.py sprites gen/sprites.png gen/sprites_meta.js 256 256
+echo **CONVERTING SPRITE SHEET TO TEXT
 python _binary_to_string.py gen/sprites.png gen/sprites_ascii.js s
+echo **BUILDING HTML FILE
 python _insert_scripts.py gen/index.html index_template.txt gen/sprites_ascii.js gen/sprites_meta.js game.js
+echo **ALL DONE
 pause
