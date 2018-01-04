b367e5e2fd1a736454b62e38e8f3ca3bc8eddedf
AjaxGb <ajaxgb@gmail.com>
2018-01-04 15:06:49 -0500

    Allow for uncompressed test build

--- src/__gen_all.bat
+++ src/__gen_all.bat
@@ -1,6 +1,8 @@
 @echo off
 mkdir gen 2> nul
 
+set DEV=1
+
 echo **GENERATING SPRITE SHEET
 python _sheetbuilder.py sprites gen/sprites.png gen/sprites_meta.js
 echo.
@@ -13,8 +15,26 @@ echo.
 echo **BUILDING HTML FILE
 python _insert_scripts.py gen/uncompressed.html index_template.txt gen/sprites_ascii.js gen/sprites_meta.js game.js
 echo.
-echo **COMPRESSING HTML FILE INTO PNG
-python _html_to_png.py gen/uncompressed.html gen/index.html
+
+if not defined DEV (
+	echo **COMPRESSING HTML FILE INTO PNG
+	python _html_to_png.py gen/uncompressed.html gen/index.html
+	call :getsize gen/index.html
+) else (
+	echo **COMPRESSING HTML FILE INTO PNG
+	python _html_to_png.py gen/uncompressed.html gen/index_c.html
+	call :getsize gen/index_c.html
+	echo.
+	echo **COPYING UNCOMPRESSED FILE FOR TESTING
+	cp gen/uncompressed.html gen/index.html
+)
+
 echo.
 echo **ALL DONE
+echo **FINAL SIZE: (( %size% KB ))
 pause
+goto :eof
+
+:getsize
+set size=%~z1
+goto :eof
